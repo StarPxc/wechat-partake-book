71f6687ddb5df1939a6d449fc4d39763ccf1ead4
Ethan <2955317305@qq.com>
2018-02-17 20:36:09 +0800

    测试login

--- page/component/user/user.js
+++ page/component/user/user.js
@@ -1,4 +1,5 @@
 // page/component/new-pages/user/user.js
+const app = getApp()
 Page({
   data:{
     thumb:'',
@@ -8,6 +9,62 @@ Page({
     address:{}
   },
   onLoad(){
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          console.log(res.code)
+          //发起网络请求
+          wx.request({
+            url: 'https://jihangyu.cn/user/login',
+            header: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            data: {
+              code: res.code
+            },
+            method: "POST",
+            success(res) {
+              console.log(res.data)
+            },
+            fail: function (res) {
+              console.log(res);
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
+
+
     var self = this;
     /**
      * 获取用户信息
@@ -17,18 +74,6 @@ Page({
         self.setData({
           thumb: res.userInfo.avatarUrl,
           nickname: res.userInfo.nickName
-        })
-      }
-    }),
-
-    /**
-     * 发起请求获取订单列表信息
-     */
-    wx.request({
-      url: 'http://www.gdfengshuo.com/api/wx/orders.txt',
-      success(res){
-        self.setData({
-          orders: res.data
         })
       }
     })
