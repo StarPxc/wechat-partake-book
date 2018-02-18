64312dc94609c82f665ccf80ccbfd08412b2016d
casual_ji <casual_ji@hotmail.com>
2018-02-18 16:06:06 +0800

    book,usbo相关接口测试完毕

--- page/component/user/user.js
+++ page/component/user/user.js
@@ -1,4 +1,4 @@
-// page/component/new-pages/user/user.js
+
 const app = getApp()
 Page({
   data:{
@@ -93,6 +93,246 @@ Page({
       }
     })
   },
+
+  addBook:function(event){
+    wx.request({
+      url: 'https://jihangyu.cn/book/addBook',
+      method:'POST',
+      data:{
+        bname: "豆腐干反对",
+        bauthor: "jhy",
+        bpublisher: "11",
+        bprice: "20",
+        bisbn: "121",
+        barcode: "213",
+        bownerid: "",
+        buserid: "oOor05dnQyVDzvRSIKsT-EzRTYgQ",
+        bstatus: "1",
+        btype: "1"
+      },
+      header: {
+        'content-type': 'application/json' ,
+        'user-token':'cba52e94877838bccd29cb47f160a299'
+      },
+      success:function(res){
+        if(res.data.code==200){
+          console.log(res)
+        }else{
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  updateBook: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/book/updateBook',
+      method: 'POST',
+      data: {
+        bid:"9",
+        bname: "是刚刚",
+        bauthor: "jhy",
+        bpublisher: "11",
+        bprice: "20",
+        bisbn: "121",
+        barcode: "213",
+        bownerid: "oOor05dnQyVDzvRSIKsT-EzRTYgQ",
+        buserid: "oOor05dnQyVDzvRSIKsT-EzRTYgQ",
+        bstatus: "1",
+        btype: "1"
+      },
+      header: {
+        'content-type': 'application/json',
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  getBookById: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/book/getBookById/8',
+      method: 'GET',
+      header: {
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  getBookByType: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/book/getBookByType/1',
+      method: 'GET',
+      header: {
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  getBookByName: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/book/getBookByName/是刚刚',
+      method: 'GET',
+      header: {
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+  
+  deleteBookById: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/book/deleteBookById/9',
+      method: 'GET',
+      header: {
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  findAllUsedBookByUid: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/ub/findAllUsedBookByUid/oOor05dnQyVDzvRSIKsT-EzRTYgQ',
+      method: 'GET',
+      header: {
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  findAllHadBookByUid: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/ub/findAllHadBookByUid/oOor05dnQyVDzvRSIKsT-EzRTYgQ',
+      method: 'GET',
+      header: {
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  orderBook: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/ub/orderBook',
+      method: 'POST',
+      data: {
+        uid: "oOor05dnQyVDzvRSIKsT-EzRTYgQ",
+        bid: "8"
+      },
+      header: {
+        'content-type': 'application/json',
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  returnBook: function (event) {
+    wx.request({
+      url: 'https://jihangyu.cn/ub/returnBook',
+      method: 'POST',
+      data: {
+        uid: "oOor05dnQyVDzvRSIKsT-EzRTYgQ",
+        bid: "8"
+      },
+      header: {
+        'content-type': 'application/json',
+        'user-token': 'cba52e94877838bccd29cb47f160a299'
+      },
+      success: function (res) {
+        if (res.data.code == 200) {
+          console.log(res)
+        } else {
+          console.log("failure")
+        }
+      }
+    })
+  },
+
+  uploadBookImg: function (event) {
+   wx.chooseImage({
+     count:1,
+     success: function(res) {
+       var tempFilePaths = res.tempFilePaths
+       console.log(tempFilePaths)
+       wx.uploadFile({
+         url: 'https://jihangyu.cn/book/uploadBookImg',
+         method: 'POST',
+         filePath: tempFilePaths[0],
+         name: 'files',
+         formData: {
+           'id': 8
+         },
+         header: {
+           'content-type': 'multipart/form-data',// 默认值
+           'user-token': 'cba52e94877838bccd29cb47f160a299'
+         },
+         success: function (res) {
+            console.log("接口调用上传成功。")
+            console.log(res.data)
+         },
+         fail: function (res){
+           console.log("接口调用上传失败。")
+           console.log(res.data)
+         }
+       })
+     },
+   })
+  },
+
+
   /**
    * 发起支付请求
    */
