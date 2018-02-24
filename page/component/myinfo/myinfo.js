7c1e17df72147cccfb40f9038abb4ac692dd5966
casual_ji <casual_ji@hotmail.com>
2018-02-24 19:49:58 +0800

    完成个人模块/个人信息

--- page/component/myinfo/myinfo.js
+++ page/component/myinfo/myinfo.js
@@ -6,14 +6,92 @@ Page({
    * 页面的初始数据
    */
   data: {
-    info: {}
+    phone:'',
+    email:'',
+    selfintroduction:'',
+    info: {},
+    show:0
   },
-
+  change(){
+    var that=this
+    that.setData({
+      show:1
+    })
+  },
+  submit(){
+    var that=this
+    var user_token = wx.getStorageSync("user_token")
+    console.log(that.data.email)
+    console.log(that.data.phone)
+    wx.request({
+      url: 'https://jihangyu.cn/user/updateUser',
+      method:'POST',
+      data:{
+        uemail:that.data.email,
+        uphone:that.data.phone,
+        uselfIntroduction: that.data.selfintroduction
+      },
+      header:{
+        'content-type': 'application/json',
+        "user-token": user_token
+      },
+      success:function(res){
+        if (res.data.code != 200) {
+          wx.showToast({
+            title: '提交失败',
+            icon: 'loading'
+          })
+        }else{
+          console.log(res.data)
+          wx.showToast({
+            title: '提交成功',
+            icon: 'success'
+          })
+        }
+      }
+    })
+  },
+  introInput(e) {
+    this.setData({
+      selfintroduction: e.detail.value
+    })
+  },
+  phoneInput(e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+  emailInput(e) {
+    this.setData({
+      email: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     var that=this
+    var user_token = wx.getStorageSync("user_token")
+    wx.request({
+      url: 'https://jihangyu.cn/user/getCurrentUser',
+      method:'GET',
+      header:{
+        'user-token':user_token
+      },
+      success:function(res){
+        if (res.data.code != 200) {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'loading'
+          })
+        }else{
+          that.setData({
+            info: res.data.data
+          })
+          // console.log(that.data.info)
+        }
+      }
+    })
     
   },
 
